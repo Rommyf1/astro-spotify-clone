b0ce8ea00d1772308cd54564ab7a4ffa8486126f
Rommy Fernández <rommyf1@gmail.com>
2024-08-23 16:38:44 -0400

    Adapted Card Play Button to work fetching to API

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -5,13 +5,25 @@ export default function CardPlayButton({ id }) {
   const { isPlaying, setIsPlaying, currentMusic, setCurrentMusic } =
     usePlayerStore((state) => state);
 
-  const handleClick = () => {
-    setCurrentMusic({ playlist: { id } });
-
-    setIsPlaying(!isPlaying);
-  };
-
   let isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
+  const handleClick = async () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(!isPlaying);
+      return;
+    }
+
+    const response = await fetch(`/api/get-info-playlist.json?id=${id}`);
+    const data = await response.json();
+    const { actualPlaylist, playlistSongs } = data;
+    setIsPlaying(true);
+    setCurrentMusic({
+      playlist: actualPlaylist,
+      song: playlistSongs[0],
+      songs: playlistSongs,
+    });
+    console.log({ actualPlaylist, playlistSongs });
+  };
 
   return (
     <button className="rounded-full p-4 bg-green-500" onClick={handleClick}>
