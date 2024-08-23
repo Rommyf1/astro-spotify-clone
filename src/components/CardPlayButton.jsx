eae2fce7cf3c715eeb06bdb1458241296d718d3b
Rommy Fernández <rommyf1@gmail.com>
2024-08-23 14:39:37 -0400

    Adapted Button to use global state

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,9 +1,21 @@
 import { PlayButton, PauseButton } from "./Player";
+import { usePlayerStore } from "../store/playerStore";
 
-export default function CardPlayButton({ isPlaying, id }) {
+export default function CardPlayButton({ id }) {
+  const { isPlaying, setIsPlaying, currentMusic, setCurrentMusic } =
+    usePlayerStore((state) => state);
+
+  const handleClick = () => {
+    setCurrentMusic({ playlist: { id } });
+
+    setIsPlaying(!isPlaying);
+  };
+
+  let isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
   return (
-    <button className="rounded-full p-4 bg-green-500">
-      {isPlaying ? <PauseButton /> : <PlayButton />}
+    <button className="rounded-full p-4 bg-green-500" onClick={handleClick}>
+      {isPlayingPlaylist ? <PauseButton /> : <PlayButton />}
     </button>
   );
 }
