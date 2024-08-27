269e3f97840f3fc40d27da91c97fa0d3467e0108
Rommy Fernández <rommyf1@gmail.com>
2024-08-27 13:19:16 -0400

    Added property 'volume' to global state to manage from Player

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 export const usePlayerStore = create((set) => ({
   isPlaying: false,
+  volume: 1,
   currentMusic: { playlist: null, song: null, songs: [] },
   setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setVolume: (volume) => set({ volume }),
   setCurrentMusic: (currentMusic) => set({ currentMusic }),
 }));
